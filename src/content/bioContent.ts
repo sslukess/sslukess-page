25af2f9a78c61c428c994ebb8a4b1958ff500b67
sslukess <lvss.dev@gmail.com>
2025-01-19 13:27:53 +0000

    update bio

--- src/content/bioContent.ts
+++ src/content/bioContent.ts
@@ -1,2 +1,2 @@
-export const content = "Hi. I am a diversely skilled software professional with over six years experience designing, building, shipping, maintaining and talking about software applications in an Agile setting. I am passionate about superior user experiences, professional software engineering, elegant solutions, high performing and fun teams, and shipping products to real world users.";
+export const content = "I am a diversely skilled software professional with over six years of experience designing, building, and talking about software applications in a professional capacity. I am passionate about superior user experience, elegant solutions, high-performing teams, and shipping actually-valuable products.";
 
